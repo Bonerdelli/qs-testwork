45633d97de010beaf7ffb14e2665dcf6f7e417df
Andrew Nekrasov <andrew@smilechat.app>
2021-08-06 22:29:18 +0300

    Replace SQLite library and test db

--- server/database.js
+++ server/database.js
@@ -1,34 +1,25 @@
-const sqlite = require('sqlite3')
-const { open } = require('sqlite')
+const Database = require('better-sqlite3')
+const treeData = require('../data/tree.json')
 
-const data = require('../data/tree.json')
+const db = new Database(':memory:')
 
-let db
-async function initializeDb() {
-  db = await open({
-    filename: ':memory:',
-    driver: sqlite.Database,
-  })
-  await db.serialize(() => {
-    // Create sample tree table
-    db.exec('CREATE TABLE tree (id INTEGER, value TEXT, parent INTEGER)')
-    // Insert sample data
-    db.run('BEGIN TRANSACTION')
-    data.forEach(item => db.run(
-      'INSERT INTO tree(id, value, parent) ' +
-      'VALUES (:id, :value, :parent)', {
-        ':id': item.id,
-        ':value': item.value,
-        ':parent': item.parent,
-      }
-    ))
-    db.run('COMMIT')
-  })
-  const test = await
+const insertMany = (insert) => db.transaction((items) => {
+  for (const item of items) insert.run(item)
+})
+
+function initDb() {
+  // Create sample tree table
+  db.exec('CREATE TABLE tree (id INTEGER, value TEXT, parent INTEGER)')
+  // Insert sample data
+  const insert = db.prepare(
+    'INSERT INTO tree(id, value, parent) ' +
+    'VALUES (@id, @value, @parent)'
+  )
+  insertMany(insert)(treeData)
 }
 
 
 module.exports = {
   db,
-  initializeDb,
+  initDb,
 }
--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
-const { initializeDb, database } = require('./database')
+const { initDb, db } = require('./database')
 
 const PORT = process.env.PORT || 3001
 const app = express()
 
 app.listen(PORT, async () => {
   try {
-    await initializeDb()
     console.log(`Server listening on ${PORT}`)
+    initDb()
+    const row = db.prepare('SELECT * FROM tree WHERE id = ?').get(1)
+    console.log('Test db', row)
   } catch (e) {
     console.error('Error', e.message)
   }
